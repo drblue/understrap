1b699b28e978fbc7a822d84b14d92d59fdb82583
Johan Nordström <johan@digitalvillage.se>
2019-04-12 09:51:25 +0200

    Custom site front page template file with support for Custom Post Type USPs.

---
+++ front-page.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The template for displaying the front page.
+ *
+ * @package understrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header();
+
+$container = get_theme_mod( 'understrap_container_type' );
+
+?>
+
+<?php get_template_part( 'global-templates/hero' ); ?>
+
+<?php get_template_part( 'global-templates/usps' ); ?>
+
+<div class="wrapper" id="page-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+		<div class="row">
+
+			<main class="site-main" id="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'loop-templates/content', 'frontpage' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</main><!-- #main -->
+
+		</div><!-- .row -->
+
+	</div><!-- #content -->
+
+</div><!-- #page-wrapper -->
+
+<?php get_footer(); ?>
